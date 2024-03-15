d90f7f24a8ea68fb74f760346dd88951cfb66a5f
liliia_vintiuk <rudapanda2016@gmail.com>
2024-03-15 22:38:12 +0200

    finaly

---
+++ index.js
@@ -0,0 +1,41 @@
+import { Galunes } from "./js/Galunes.js";
+
+const listDOM = document.getElementById('list');
+const formDOM = document.forms[0];
+const inputDOM = document.getElementById('task');
+
+const localStorageKey = 'tasks';
+//const localTasks = localStorage.getItem(localStorageKey);
+const taskList = [];
+
+let HTML = '';
+
+for (const item of taskList) {
+    HTML += `<li>${item}</li>`;
+}
+
+listDOM.innerHTML = HTML;
+
+formDOM.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const text = inputDOM.value;
+    const word = new Galunes(text);
+    const changeWord = word.pirma();
+    
+    listDOM.insertAdjacentHTML(
+        'beforeend',
+        `<li>${text} - ${changeWord}</li>` 
+        
+    );
+
+
+    localStorage.setItem(localStorageKey, JSON.stringify(taskList));
+    
+});
+
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+  document.location.reload();
+});
+
---
+++ js/Galunes.js
@@ -0,0 +1,61 @@
+class Galunes{
+    constructor(word){
+        this.word = word;
+    }
+    
+    pirma() {
+        if (this.word.endsWith('as')) {
+            return this.word.slice(0, -2) + 'e';
+        }
+        if (this.word.endsWith('is')) {
+            return this.word.slice(0, -2) + 'i';
+        }
+        if (this.word.endsWith('ys')) {
+            return this.word.slice(0, -2) + 'y';
+        }
+           
+        if (this.word.endsWith('ė')) {
+            return this.word.slice(0, -1) + 'e';
+        }
+   
+        if (this.word.endsWith('is')) {
+            return this.word.slice(0, -2) + 'ie';
+        }
+    
+        if (this.word.endsWith('us')) {
+            return this.word.slice(0, -2) + 'au';
+        }
+    
+        if (this.word.endsWith('uo')) {
+            return this.word.slice(0, -2) + 'ienie';
+        }
+        return this.word;
+    }
+}
+
+export { Galunes}
+
+/*antra() {
+    if (this.word.endsWith('ė')) {
+        return this.word.slice(0, -1) + 'e';
+    }
+}
+
+trecia() {
+    if (this.word.endsWith('is')) {
+        return this.word.slice(0, -2) + 'ie';
+    }
+}
+
+ketvirta() {
+    if (this.word.endsWith('us')) {
+        return this.word.slice(0, -2) + 'au';
+    }
+}
+
+penkta() {
+    if (this.word.endsWith('uo')) {
+        return this.word.slice(0, -2) + 'ienie';
+    }
+}
+}*/
